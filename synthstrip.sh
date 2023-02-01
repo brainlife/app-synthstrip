27707382ccbe4a3a4bc35479b93796d1382fb006
Brad Caron <bacaron245@gmail.com>
2023-02-01 12:23:37 -0600

    reason: commits for dwi

--- synthstrip.sh
+++ synthstrip.sh
@@ -3,7 +3,11 @@
 set -ex
 
 # configurable inputs
-input=`jq -r '.input' config.json`
+dwi=`jq -r '.input' config.json`
+input=nodif.nii.gz
+input_type=dwi
+bvals=`jq -r '.bvals' config.json`
+bvecs=`jq -r '.bvecs' config.json`
 border=`jq -r '.border' config.json`
 
 # make output directories
@@ -14,11 +18,17 @@ outdir_mask=output_mask
 [ ! -d ${outdir_mask} ] && mkdir -p ${outdir_mask}
 
 # run synthstrip to get mask and brain extracted image
-[ ! -f ${outdir_t1}/t1.nii.gz ] && mri_synthstrip -i ${input} -o ${outdir_t1}/t1.nii.gz -m ${outdir_mask}/mask.nii.gz
+[ ! -f ${outdir_mask}/mask.nii.gz ] && mri_synthstrip -i ${input} -o tmp.nii.gz -m ${outdir_mask}/mask.nii.gz
+
+# mask dwi volumes
+[ ! -f ${outdir_t1}/${input_type}.nii.gz ] && mri_mask ${dwi} ${outdir_mask}/mask.nii.gz ${outdir_t1}/${input_type}.nii.gz
 
 # final check
 if [ -f ${outdir_mask}/mask.nii.gz ]; then
 	echo "complete"
+	cp ${bvals} ${outdir_t1}/${input_type}.bvals
+	cp ${bvecs} ${outdir_t1}/${input_type}.bvecs
+	rm -rf tmp.nii.gz nodif.nii.gz
 	exit 0
 else
 	echo "something went wrong. check logs"
