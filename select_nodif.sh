38411b719ad4adae4d44fe8aef18beb552237239
Brad Caron <bacaron245@gmail.com>
2023-02-01 12:46:41 -0600

    Update select_nodif.sh

--- select_nodif.sh
+++ select_nodif.sh
@@ -4,6 +4,6 @@ set -ex
 dwi=`jq -r '.input' config.json`
 bvals=`jq -r '.bvals' config.json`
 
-[ ! -f nodif.nii.gz ] && select_dwi_vols ${dwi} ${bvals} nodif.nii.gz 0 -m
+[ ! -f ./nodif.nii.gz ] && select_dwi_vols ${dwi} ${bvals} ./nodif.nii.gz 0 -m
 
-[ ! -f nodif.nii.gz ] && echo "something went wrong. check logs" && exit 1
+[ ! -f ./nodif.nii.gz ] && echo "something went wrong. check logs" && exit 1
