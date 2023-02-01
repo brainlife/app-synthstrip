e38bb006573e626eca489b52fe958fc45ab5979b
Brad Caron <bacaron245@gmail.com>
2023-02-01 12:51:14 -0600

    Update select_nodif.sh

--- select_nodif.sh
+++ select_nodif.sh
@@ -4,5 +4,3 @@ set -ex
 func=`jq -r '.input' config.json`
 
 [ ! -f ./nodif.nii.gz ] && fslselectvols -i ${func} -o ./nodif.nii.gz --vols=0
-
-[ ! -f ./nodif.nii.gz ] && echo "something went wrong. check logs" && exit 1
